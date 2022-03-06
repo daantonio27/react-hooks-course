04dff00d1939d0d54b677323df1cad8edfb8e25a
daantonio <daantonioc3@gmail.com>
2022-03-06 23:27:11 +0000

    RefTutorial & LayoutEffectTutorial

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import EffectTutorial from "./UseEffect/EffectTutorial";
 import ReducerTutorial from "./UseReducer/ReducerTutorial";
 import StateTutorial from "./UseState/StateTutorial";
+import RefTutorial from "./UseRef/RefTutorial";
+import LayoutEffectTutorial from "./UseLayoutEffect/LayoutEffectTutorial";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <StateTutorial />
       <ReducerTutorial />
       <EffectTutorial />
+      <RefTutorial />
+      <LayoutEffectTutorial />
     </div>
   );
 }
